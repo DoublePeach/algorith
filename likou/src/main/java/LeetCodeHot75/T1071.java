056e22d392cf705b437b1aad245284f30e5b0caa
Tovin <taotao.hao@sfygroup.com>
2025-08-15 15:08:51 +0800

    2025年8月15日力扣刷题-1071. 字符串的最大公因子

---
+++ likou/src/main/java/LeetCodeHot75/T1071.java
@@ -0,0 +1,9 @@
+package LeetCodeHot75;
+
+public class T1071 {
+    public String gcdOfStrings(String str1, String str2) {
+        // 逆天，这道题是用数学推导的方式解
+        // gcd算法秒了，就不写题解过程了
+        return null;
+    }
+}
